1a37b1f9a621eb9d7ceea51ff7c3e547d3510338
TDjamik <abuabdulaziz3031@gmail.com>
2022-04-05 01:43:22 +0300

    Initial commit

---
+++ bin/models/phone_abstract_class.dart
@@ -0,0 +1,14 @@
+abstract class Phone{
+  late String model;
+  late int id;
+  late String madeIn;
+
+
+  bool isHaveSimCard();// sim cartasi bormi?
+  bool isOnActive();// tel yoqig'mi?
+  void call();// tel qilish
+  void sendSms();// sms yuvorish
+  void addContact();// contact qo'shish
+  void delete();// contact o'chirish
+
+}
