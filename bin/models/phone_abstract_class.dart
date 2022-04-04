a2f78d3511eed4833c11fe1671e2b250be046e4f
TDjamik <abuabdulaziz3031@gmail.com>
2022-04-05 01:43:22 +0300

    mixin

--- bin/models/phone_abstract_class.dart
+++ bin/models/phone_abstract_class.dart
@@ -1,14 +1,17 @@
-abstract class Phone{
+abstract class Phone {
   late String model;
   late int id;
   late String madeIn;
 
+  bool isHaveSimCard(); // sim cartasi bormi?
+  bool isOnActive(); // tel yoqig'mi?
+  void call(); // tel qilish
+  void sendSms(); // sms yuvorish
+  void addContact(); // contact qo'shish
+  void delete(); // contact o'chirish
+}
 
-  bool isHaveSimCard();// sim cartasi bormi?
-  bool isOnActive();// tel yoqig'mi?
-  void call();// tel qilish
-  void sendSms();// sms yuvorish
-  void addContact();// contact qo'shish
-  void delete();// contact o'chirish
-
+mixin PhoneMethods{
+  void goToAppStore();
+  void goToPlayMarket();
 }
