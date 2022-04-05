c051270e645d656dce112913b1572fa7fe6348c5
Sunnat03 <sunnatmaxmayusupov02@gmail.com>
2022-04-05 13:55:56 +0500

    Sunnat

--- bin/models/xiaomi_class.dart
+++ bin/models/xiaomi_class.dart
@@ -1,13 +1,48 @@
+import 'dart:io';
+
 import 'phone_abstract_class.dart';
 
 class Xiaomi extends Phone with PhoneSystem {
+  late String name;
+  late String number;
+  String? txt;
   Xiaomi(String model, int id, String madeIn) : super(model, id, madeIn);
 
   @override
-  void addContact() {}
+  void addContact() {
+    stdout.write("Iltimos kontakt qoshish uchun isim kirgizing => ");
+    name = stdin.readLineSync()!;
+  if(name.length>1){
+    this.name =name;
+  }else{
+    throw Exception("ERROR NAME");
+  }
+   stdout.write("Iltimos kontakt qoshish uchun raqamni ham kirgizing => ");
+    number = stdin.readLineSync()!;
+  if(number.length>10 && number.contains("+")){
+    this.number = number;
+    print("Raqam muvafaqiyatli qo\'shildi");
+  }else {
+    throw Exception("ERROR NUMBER");
+  }
+
+  }
 
   @override
-  void call() {}
+  void call() {
+    stdout.write("Iltimos qo\'ng\'iroq qilmoqchi bo\'lgan raqamizni kirgizin => ");
+    number = stdin.readLineSync()!;
+    if(number.length>10 && number.contains("+")){
+      this.number = number;
+      print("$number");
+      print("Calling");
+      sleep(Duration(seconds: 5));
+      print("asked");
+    }else {
+      throw Exception("ERROR NUMBER");
+    };
+
+  }
 
   @override
   void delete() {}
@@ -19,11 +54,16 @@ class Xiaomi extends Phone with PhoneSystem {
 
   @override
   bool isOnActive() {
-    throw UnimplementedError();
+    return true;
   }
 
   @override
-  void sendSms() {}
+  void sendSms() {
+    stdout.write("Sms yozish uchun text kirgizing => ");
+    txt = stdin.readLineSync();
+   print("Send sms"); sleep(Duration(seconds: 5));
+    print("Seen sms");
+  }
 
   @override
   void show() {}
